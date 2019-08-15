363b8ad9d84ec16d887b5073a62e615c78145e71
ed1yang <qitian@126.com>
2019-08-15 16:56:09 +1200

    0815

--- map/src/App.js
+++ map/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+// import IconAvatars from './IconAvatars.js';
+// import {Test} from './test.js'
+
 
 class App extends React.Component {
   constructor(props) {
@@ -16,15 +19,23 @@ class App extends React.Component {
     return console.log('X is: '+this.state.x+' Y is: '+this.state.y);
   }
 
+  // localtest(){
+  //   return 'wow';
+  // }
+
+  // importTest(){
+  //   this._child.Test();
+  // }
+
   render() {
     const { x, y } = this.state;
     return <div className="container">
       <div >
         <img onMouseMove={this._onMouseMove} 
           alt='map'
-          width="100" 
-          height="150" 
-          src="http://www.mariogiannini.com/wp-content/uploads/2017/10/Photo-200x300.jpg"
+          width="50%" 
+          height="50%" 
+          src="https://www.livebakerblock.com/wp-content/uploads/2017/07/baker-plan-c1-1600px.png"
           onClick={this.handleOnClick}
         />
       </div>
---
+++ map/src/test.js
@@ -0,0 +1,4 @@
+function Test(){
+    return "test text";
+}
+export default Test;
